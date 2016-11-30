6dee061a2ac444d767c8c98d7237fc45bcb931d3
jsromero27 <jsromero27@gmail.com>
2016-11-30 08:18:11 -0600

    Delete dbconfig.php

--- dbconfig.php
+++
@@ -1,8 +0,0 @@
-?<php
-$dbhost = "mysql.hostinger.co.uk";
-$dbuser = "u836410390_qejyp";
-$dbpass = "MFfAUx9BzS";
-$dbname = "u836410390_jyrem";
-mysql_connect($dbhost,$dbuser,$dbpass) or die('cannot connect to the server'); 
-mysql_select_db($dbname) or die('database selection problem');
-?>
